8a035eebc9ff4565397ade2b362e2647a0841372
clear <solankiashok667@gmail.com>
2023-07-02 09:10:56 +0530

    created a new react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'App.css'
+
+
+function App() {
+  return (
+    <div className='header'>
+           <h1>
+             hello how are you 
+           </h1>
+      </div>
+  )
+}
+
+export default App
